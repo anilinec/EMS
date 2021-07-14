f372ec10b44f2d53bde57f82c04c2a1def41278e
anilinec <mehmetanilinec@gmail.com>
2021-07-13 21:22:39 -0700

    Hi

---
+++ EmployeeManagementSystem/src/com/ems/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,14 @@
+package com.ems.exceptions;
+
+public class EmployeeNotFoundException extends Exception {
+    private int id;
+
+    public EmployeeNotFoundException(int id) {
+        this.id = id;
+    }
+
+   
+	public String toString(){
+        return "Employee Not Found Exception";
+    }
+}
