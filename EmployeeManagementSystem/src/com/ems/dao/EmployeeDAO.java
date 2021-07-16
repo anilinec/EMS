db2f0907452dbd89d0b2759286ed85a261788f4f
anilinec <mehmetanilinec@gmail.com>
2021-07-15 19:24:02 -0700

    hi

--- EmployeeManagementSystem/src/com/ems/dao/EmployeeDAO.java
+++ EmployeeManagementSystem/src/com/ems/dao/EmployeeDAO.java
@@ -11,14 +11,12 @@ import java.util.List;
 public interface EmployeeDAO {
     public abstract void addEmployee(Employee e);
     public abstract void deleteEmployee(int id);
+    public abstract void updateEmployee(Employee e);
     public abstract Employee findEmployee(Employee e) throws EmployeeNotFoundException;
     public List<Employee> showAllEmployees();
-    public abstract void updateEmployee(String name, int id);
 
     public static final String URL = "jdbc:mysql://localhost:3306/employees_database";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "159753";
+    public static final String PASSWORD = "";
     public static final String DRIVER_CLASSNAME = "com.mysql.jdbc.Driver";
-	
-
 }
--- EmployeeManagementSystem/src/com/ems/service/EmployeeService.java
+++ EmployeeManagementSystem/src/com/ems/service/EmployeeService.java
@@ -8,14 +8,13 @@ import java.util.List;
 public interface EmployeeService {
     public abstract void addEmployee(Employee e);
     public abstract void deleteEmployee(int id);
+
+    public abstract void updateEmployee(Employee e);
     public abstract Employee findEmployee(Employee e) throws EmployeeNotFoundException;
     public List<Employee> showAllEmployees();
-    
-    
+
     public static final String URL = "jdbc:mysql://localhost:3306/employees_database";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "159753";
+    public static final String PASSWORD = "";
     public static final String DRIVER_CLASSNAME = "com.mysql.jdbc.Driver";
-	
-	public abstract void updateEmployee(String name, int id);
 }
